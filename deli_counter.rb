5063a4e39aefb786c168bb9b5151a2a4dfc67c68
Paolo Ventura <ventura.paolo@gmail.com>
2018-09-24 14:30:57 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -14,10 +14,11 @@ end
 
 #. Build a method that a new customer will use when entering the deli. The `take_a_number` method should accept two arguments, the array for the current line of people (`katz_deli`), and a string containing the name of the person joining the end of the line. The method should call out (`puts`) the person's name along with their position in line. **Top-Tip:** *Remember that people like to count from* `1`*, not from* `0` *("zero") like computers.*
 
-
+counter = 0
 def take_a_number(katz_deli)
-    counter = katz_deli.length 
-    puts "Welcome, #{new_person}. You are number #{lengths} in line."
+    
+    counter += 1
+    puts "You are number #{counter} in line."
 end
 
 #Solution from !Solution - More refactored
